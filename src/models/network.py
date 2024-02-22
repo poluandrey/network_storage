fde3660484fc564e7565091efd6c58331982198e
apolumestny <anpolumestny@gmail.com>
2024-02-22 14:20:33 +0300

    network crud

--- src/models/network.py
+++ src/models/network.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 
 from src.database.base import Base
@@ -8,7 +9,11 @@ class Network(Base):
     __tablename__ = 'network'
 
     id = Column(Integer, primary_key=True)
-    parent_id = Column(Integer)
+    parent_id = Column(Integer, ForeignKey('network.id'), nullable=True)
     network = Column(String, unique=True)
     create_at = Column(DateTime(timezone=True), server_default=func.now())
     last_update = Column(DateTime(timezone=True), onupdate=func.now())
+
+    sub_networks = relationship("Network", back_populates='parent_network')
+    parent_network = relationship("Network", remote_side=[id], back_populates='sub_networks')
+
